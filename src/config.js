6a0297611bfe877ef4c93bda54546b6f9f808b6a
sebseb7 <sebgreenbus@gmail.com>
2025-05-20 22:50:54 +0200

    Update project name from GrowBNB to SeedHeads in index.html and config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  shopName: 'GrowBNB',
+  shopName: 'SeedHeads',
   apiBaseUrl: 'http://10.10.10.43:9303',
   fallbackUrl: 'https://growbnb.de/',
   // Add other configuration values here as needed
